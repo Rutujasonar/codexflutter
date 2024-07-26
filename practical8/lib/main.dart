428b4038f45f45c28bde1efd13d994688b1ccfd1
rutuja <rutujafeb20@gmail.com>
2024-07-26 23:57:20 +0530

    completed

---
+++ practical8/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:practical8/Screen1.dart';
+import 'package:practical8/renthouse.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false, home: RentHouse());
+  }
+}
